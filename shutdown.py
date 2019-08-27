ab62a99df11009911a060de0161fdfe166465d10
Aditya Ajay <aditya.ajay@outlook.in>
2019-08-27 16:56:25 +0530

    modified pin for shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -1,16 +1,18 @@
+#!/usr/bin/env python3
+# to execute at startup
 import RPi.GPIO as GPIO
 import time
 import os
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def Shutdown(channel):
     os.system("sudo shutdown -h now")
 
 
-GPIO.add_event_detect(18, GPIO.FALLING, callback=Shutdown, bouncetime=2000)
+GPIO.add_event_detect(26, GPIO.FALLING, callback=Shutdown, bouncetime=2000)
 
 while 1:
     time.sleep(5)
